f91f202b6bc1457308e6483ad6cdec0f7ec42480
JiangZhenxiang <1036615730@qq.com>
2019-12-31 09:09:49 +0800

    Class Test

---
+++ RTest/t1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int n,m;
+    char get[100][101];
+    scanf("%d %d",&n,&m);
+    for (int i=0;i<n;i++) {
+        if (i + m < n) {
+            scanf("%s", get[i+m]);
+        }
+        else {
+            scanf("%s",get[i+m-n]);
+        }
+    }
+    for (int i=0;i<n;i++){
+        printf("%s ",get[i]);
+    }
+    return 0;
+}
